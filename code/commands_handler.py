f72ed86466218e9aa8a7be9a604405678a8977a7
Omer Hadad <omer@paranagames.com>
2024-07-17 00:45:15 +0300

    Added basic queue

--- code/commands_handler.py
+++ code/commands_handler.py
@@ -5,18 +5,20 @@ songsQueue : list[str] = []
 
 async def handle_command(ctx: Context, input: str):
     if input.startswith('play'):
-        url = get_song_url_from_input(input)
-        print(f'URL: {url}')
-        print(f'Ctx: {ctx == None} {ctx.voice_client == None}')
-        if(is_song_playing(ctx)): add_song_to_queue(url)
-        else: await play_song(ctx, url)
-    elif input.startswith('pause'):
-        await ctx.send('Paused the song')
+        await handle_command_play(ctx, input)
     elif input.startswith('stop'):
+        songsQueue.clear()
         return await stop(ctx)
     elif input[0] == '!':
         raise Exception('No command found for this input')
     else: raise Exception('')
+    
+async def handle_command_play(ctx: Context, input: str):
+    url = get_song_url_from_input(input)
+    if(is_song_playing(ctx)): await add_song_to_queue(url, ctx)
+    else: await play_song(ctx, url)
+
+
     
 def get_song_url_from_input(input: str) -> str:
      url = input[5:]
@@ -25,15 +27,22 @@ def get_song_url_from_input(input: str) -> str:
     
 async def add_song_to_queue(song_url: str, ctx: Context):
     songsQueue.append(song_url)
+    print(f'Added the song to the queue, queue count: {len(songsQueue)}')
     await ctx.send(f'Added the song to the queue')
     
 async def play_song(ctx: Context, url: str):
     try:
        await ctx.send('Playing the song')
-       await play_audio(ctx, url)
+       await play_audio(ctx, url, on_song_end)
     except Exception as e:
         await ctx.send(f'An error occurred: {e} leaving voice channel')
         await leave_voice_channel(ctx)
+
+async def on_song_end(ctx: Context):
+    print(f'Song ended checking next in queue count is:{len(songsQueue)}')
+    if(len(songsQueue) == 0): return
+    url = songsQueue.pop(0)
+    await play_song(ctx, url)
         
 async def stop(ctx: Context):
     await leave_voice_channel(ctx)
--- code/ytdlp_handler.py
+++ code/ytdlp_handler.py
@@ -1,17 +1,10 @@
+import asyncio
 import discord
 import yt_dlp
 
-def is_song_playing(ctx):
-    return ctx.voice_client != None and ctx.voice_client.is_playing()
+FFMPEG_OPTIONS =FFMPEG_OPTIONS ={'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn -filter:a "volume=0.5"'}
 
-async def play_audio(ctx, url):
-    voice_channel = ctx.author.voice.channel
-    if ctx.voice_client is None:
-        await voice_channel.connect()
-
-    FFMPEG_OPTIONS =FFMPEG_OPTIONS ={'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn -filter:a "volume=0.5"'}
-    
-    ydl_opts = {
+ydl_opts = {
     'format': 'bestaudio/best',
     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
     'quiet': True,
@@ -20,7 +13,24 @@ async def play_audio(ctx, url):
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    }
+}
+
+def is_song_playing(ctx):
+    return ctx.voice_client != None and ctx.voice_client.is_playing()
+
+async def play_audio(ctx, url,on_song_end):
+    voice_channel = ctx.author.voice.channel
+    if ctx.voice_client is None:
+        await voice_channel.connect()
+
+    def after_playing(error):
+        if error:
+          print(f"An error occurred: {error}")
+        else:
+            print("Song has finished playing.")
+        # Call the on_song_end function here
+        asyncio.run_coroutine_threadsafe(on_song_end(ctx), ctx.bot.loop)
+    
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         secret = url
         info = ydl.extract_info(secret, download=False)
@@ -28,8 +38,8 @@ async def play_audio(ctx, url):
         print(url2)
         source = discord.FFmpegOpusAudio(url2, **FFMPEG_OPTIONS)
         vc = ctx.voice_client
-        vc.play(source)
-        
+        vc.play(source, after=after_playing)
+            
 async def leave_voice_channel(ctx):
     if ctx.voice_client:
         await ctx.guild.voice_client.disconnect()
