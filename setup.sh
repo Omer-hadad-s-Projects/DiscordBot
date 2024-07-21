095941beda2cfb1cb8e6a7d6ff44d96db2be25ca
Omer Hadad <omer@paranagames.com>
2024-07-22 00:14:02 +0300

    Added fix to platform on venv open

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
+activate_venv() {
+  if [ -f ".venv/bin/activate" ]; then
+    # For bash and zsh
+    . .venv/bin/activate
+  else
+    echo "Virtual environment not found in .venv directory."
+    exit 1
+  fi
+}
+
 sh discord_token_saver.sh
 python3 -m venv .venv
-source .venv/bin/activate
+activate_venv
 pip3 install -U pip
 pip3 install -U python-dotenv
 pip3 install -U discord
